b0212336d85669aff980eba54a234ce0fc2454bd
Stas Seliverstov <stasseliverstov@MacBook-Pro-Stas.local>
2017-02-03 17:48:03 +0300

    Fix test for parameters in steps (fixes #29)

--- allure-pytest/test/steps/step_parameters_test.py
+++ allure-pytest/test/steps/step_parameters_test.py
@@ -10,7 +10,7 @@ def step_with_parameters(step_fail, default_value=777, named_parameter=888):
     assert not step_fail
 
 
-def test_outside_step_with_parameters():
+def test_step_with_parameters():
     """
     >>> allure_report = getfixture('allure_report')
     >>> assert_that(allure_report,
