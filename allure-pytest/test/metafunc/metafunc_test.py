4630e7b823a2d56e25a7ea757aa1b56af0e6102c
Stas Seliverstov <stasseliverstov@MacBook-Pro-Stas.local>
2017-02-10 00:59:56 +0300

    parametrize with metafunc (fixes #37)

---
+++ allure-pytest/test/metafunc/metafunc_test.py
@@ -0,0 +1,40 @@
+"""
+>>> allure_report = getfixture('allure_report')
+
+>>> assert_that(allure_report,
+...             all_of(
+...                 has_property('test_cases', has_length(2)),
+...                 has_property('test_groups', has_length(0))
+...             ))
+"""
+
+
+def test_metafunc_param(metafunc_param):
+    """
+    >>> allure_report = getfixture('allure_report')
+    >>> assert_that(allure_report,
+    ...             has_test_case('test_metafunc_param[True]',
+    ...                           has_parameter('metafunc_param', True)
+    ...             )
+    ... )
+    """
+    assert metafunc_param
+
+
+def test_metafunc_param_with_ids(metafunc_param_with_ids):
+    """
+    >>> allure_report = getfixture('allure_report')
+
+    >>> assert_that(allure_report,
+    ...             has_test_case('test_metafunc_param_with_ids[metafunc_param_id]',
+    ...                           has_parameter('metafunc_param_id', True)
+    ...             )
+    ... ) # doctest: +SKIP
+
+    >>> assert_that(allure_report,
+    ...             has_test_case('test_metafunc_param_with_ids[metafunc_param_id]',
+    ...                           has_parameter('metafunc_param_with_ids', True)
+    ...             )
+    ... )
+    """
+    assert metafunc_param_with_ids
