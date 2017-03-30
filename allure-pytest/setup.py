5dc64564ffc1b78639ef127075ba32d33d0fb106
Stas Seliverstov <stasseliverstov@MacBook-Pro-Stas.local>
2017-03-30 19:19:01 +0300

    change version and add description

--- allure-pytest/setup.py
+++ allure-pytest/setup.py
@@ -1,20 +1,37 @@
 from setuptools import setup
 
-PACKAGE = "pytest-allure-adaptor"
-VERSION = "2.0.4"
+PACKAGE = "allure-pytest"
+VERSION = "2.0.0b1"
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Framework :: Pytest',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Topic :: Software Development :: Quality Assurance'
+    'Topic :: Software Development :: Testing',
+]
 
 install_requires = [
     "pytest>=2.7.3",
-    "allure-python-commons==2.0.4"
+    "allure-python-commons==2.0.0b1"
 ]
 
 def main():
     setup(
         name=PACKAGE,
         version=VERSION,
-        packages=['allure'],
-        package_dir={'allure': 'src/allure'},
-        entry_points={'pytest11': ['allure_adaptor = allure.pytest_plugin']},
+        description="Common module for integrate allure with python-based frameworks",
+        long_description="Coming soon...",
+        url="https://github.com/allure-framework/allure-python2",
+        author="QAMetaSoftware, Stanislav Seliverstov",
+        author_email="sseliverstov@qameta.io",
+        license="Apache-2.0",
+        classifiers=classifiers,
+        keywords="",
+        packages=["allure"],
+        package_dir={"allure": "src/allure"},
+        entry_points={"pytest11": ["allure_pytest = allure.pytest_plugin"]},
         install_requires=install_requires
     )
 
--- allure-python-commons/setup.py
+++ allure-python-commons/setup.py
@@ -2,8 +2,15 @@ import sys
 from setuptools import setup
 
 PACKAGE = "allure-python-commons"
-VERSION = "2.0.4"
+VERSION = "2.0.0b1"
 
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Topic :: Software Development :: Quality Assurance'
+    'Topic :: Software Development :: Testing',
+]
 
 install_requires = [
     "attrs>=16.0.0",
@@ -17,8 +24,16 @@ def main():
     setup(
         name=PACKAGE,
         version=VERSION,
-        packages=['allure_commons'],
-        package_dir={'allure_commons': 'src'},
+        description="Common module for integrate allure with python-based frameworks",
+        long_description="Coming soon...",
+        url="https://github.com/allure-framework/allure-python2",
+        author="QAMetaSoftware, Stanislav Seliverstov",
+        author_email="sseliverstov@qameta.io",
+        license="Apache-2.0",
+        classifiers=classifiers,
+        keywords="",
+        packages=["allure_commons"],
+        package_dir={"allure_commons": "src"},
         install_requires=install_requires
     )
 
