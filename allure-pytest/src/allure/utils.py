5c05bf3a5632d0e681ae4fa1f39754b9e444ac6b
mio4kon <yibo.yin@ele.me>
2017-06-05 18:09:35 +0800

    getargspec is override,change to getfullargspec

--- allure-pytest/src/allure/utils.py
+++ allure-pytest/src/allure/utils.py
@@ -84,8 +84,8 @@ def allure_full_name(nodeid):
 
 
 def step_parameters(func, *a, **kw):
-    all_names = inspect.getargspec(func).args
-    defaults = inspect.getargspec(func).defaults
+    all_names = inspect.getfullargspec(func).args
+    defaults = inspect.getfullargspec(func).defaults
     args_part = [(n, str(v)) for n, v in zip(all_names, a)]
     kwarg_part = [(n, str(kw[n]) if n in kw else str(defaults[i])) for i, n in enumerate(all_names[len(a):])]
     return args_part + kwarg_part
