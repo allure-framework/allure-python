ab0235b2d7240cec7be56b315f3d3578df757408
Stanislav Seliverstov <s.seliverstov@gmail.com>
2017-07-26 17:06:14 +0300

    run behave selftest in separated thread (connects #88 via #122)

--- allure-behave/features/steps/behave_steps.py
+++ allure-behave/features/steps/behave_steps.py
@@ -6,6 +6,7 @@ from behave.runner import ModelRunner
 from behave.configuration import Configuration
 from behave.formatter._registry import make_formatters
 from behave.formatter.base import StreamOpener
+import threading
 
 
 @given(u'feature definition')
@@ -18,15 +19,21 @@ def feature_definition(context, **kwargs):
 @when(u'I run behave with allure formatter')
 @when(u'I run behave with allure formatter with options "{args}"')
 def run_behave_with_allure(context, **kwargs):
-    cmd_args = '-v -f allure_behave.formatter:AllureFormatter -f pretty'
-    cmd = '{options} {cmd}'.format(cmd=cmd_args, options=kwargs.get('args', ''))
-    config = Configuration(command_args=cmd)
 
-    result_tmp_dir = mkdtemp(dir=os.environ.get('TEST_TMP', None))
-    stream_opener = StreamOpener(filename=result_tmp_dir)
+    def run(context, **kwargs):
+        cmd_args = '-v -f allure_behave.formatter:AllureFormatter -f pretty'
+        cmd = '{options} {cmd}'.format(cmd=cmd_args, options=kwargs.get('args', ''))
+        config = Configuration(command_args=cmd)
 
-    model_runner = ModelRunner(config, [context.feature_definition])
-    model_runner.formatters = make_formatters(config, [stream_opener])
-    model_runner.run()
+        result_tmp_dir = mkdtemp(dir=os.environ.get('TEST_TMP', None))
+        stream_opener = StreamOpener(filename=result_tmp_dir)
 
-    context.allure_report = AllureReport(result_tmp_dir)
+        model_runner = ModelRunner(config, [context.feature_definition])
+        model_runner.formatters = make_formatters(config, [stream_opener])
+        model_runner.run()
+
+        context.allure_report = AllureReport(result_tmp_dir)
+
+    behave_tread = threading.Thread(target=run, args=(context,), kwargs=kwargs)
+    behave_tread.start()
+    behave_tread.join()
--- allure-python-commons/src/_core.py
+++ allure-python-commons/src/_core.py
@@ -1,13 +1,25 @@
 import threading
+from six import with_metaclass
 from pluggy import PluginManager
 from allure_commons import _hooks
 
 
-_storage = threading.local()
-_storage.plugin_manager = PluginManager('allure')
-_storage.plugin_manager.add_hookspecs(_hooks.AllureUserHooks)
-_storage.plugin_manager.add_hookspecs(_hooks.AllureDeveloperHooks)
+class MetaPluginManager(type):
+    _storage = threading.local()
+
+    @staticmethod
+    def get_plugin_manager():
+        if not hasattr(MetaPluginManager._storage, 'plugin_manager'):
+            MetaPluginManager._storage.plugin_manager = PluginManager('allure')
+            MetaPluginManager._storage.plugin_manager.add_hookspecs(_hooks.AllureUserHooks)
+            MetaPluginManager._storage.plugin_manager.add_hookspecs(_hooks.AllureDeveloperHooks)
+
+        return MetaPluginManager._storage.plugin_manager
+
+    def __getattr__(cls, attr):
+        pm = MetaPluginManager.get_plugin_manager()
+        return getattr(pm, attr)
 
 
-plugin_manager = _storage.plugin_manager
-register = plugin_manager.register
+class plugin_manager(with_metaclass(MetaPluginManager)):
+    pass
