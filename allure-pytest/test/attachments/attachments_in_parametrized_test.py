6c1817be6ee40f23f19363ad3f1d168bc26ece75
Stas Seliverstov <s.seliverstov@gmail.com>
2017-08-05 02:20:00 +0300

    fixes

--- allure-pytest/test/attachments/attachments_in_parametrized_test.py
+++ allure-pytest/test/attachments/attachments_in_parametrized_test.py
@@ -11,15 +11,18 @@
 import allure
 import pytest
 
+BODY = ['I Like to', 'Move It']
 
-@pytest.mark.parametrize('attachment', ['body'])
+
+@pytest.mark.parametrize('attachment', BODY)
 def test_attach_data_from_parametrized_test(attachment):
     """
     >>> allure_report = getfixture('allure_report')
-    >>> assert_that(allure_report,
-    ...             has_test_case('test_attach_data_from_parametrized_test',
-    ...                           has_attachment()
-    ...             )
-    ... )
+    >>> for body in BODY:
+    ...     assert_that(allure_report,
+    ...                 has_test_case('test_attach_data_from_parametrized_test[{body}]'.format(body=body),
+    ...                              has_attachment()
+    ...                )
+    ...     )
     """
     allure.attach(attachment)
