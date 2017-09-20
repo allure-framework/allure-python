4d567780a139b96164838c8dcdd01f56f7aa5ae1
Stas Seliverstov <s.seliverstov@gmail.com>
2017-09-20 22:26:36 +0300

    bump-version

--- allure-pytest/setup.py
+++ allure-pytest/setup.py
@@ -14,7 +14,7 @@ except (DistributionNotFound, VersionConflict):
     pass
 
 PACKAGE = "allure-pytest"
-VERSION = "2.2.2b1"
+VERSION = "2.2.2b2"
 
 classifiers = [
     'Development Status :: 4 - Beta',
@@ -28,7 +28,7 @@ classifiers = [
 install_requires = [
     "pytest>=2.7.3",
     "six>=1.9.0",
-    "allure-python-commons==2.2.2b1"
+    "allure-python-commons==2.2.2b2"
 ]
 
 
