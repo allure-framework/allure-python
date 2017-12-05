bb5c2d96116c9f017d217510cd8e33cfd6ede1b0
ProSerg <primatvko@gmail.com>
2017-12-05 14:26:56 +0300

    Update listener.py
    added exception for getting finalizer #181.

--- allure-pytest/src/listener.py
+++ allure-pytest/src/listener.py
@@ -128,10 +128,14 @@ class AllureListener(object):
                                                stop=now(),
                                                status=get_outcome_status(outcome),
                                                statusDetails=get_outcome_status_details(outcome))
-
-        for index, finalizer in enumerate(fixturedef._finalizer or ()):
-            name = '{fixture}::{finalizer}'.format(fixture=fixturedef.argname, finalizer=finalizer.__name__)
-            fixturedef._finalizer[index] = allure_commons.fixture(finalizer, parent_uuid=container_uuid, name=name)
+        try:
+            for index, finalizer in enumerate(fixturedef._finalizer or ()):
+                name = '{fixture}::{finalizer}'.format(fixture=fixturedef.argname, finalizer=finalizer.__name__)
+                fixturedef._finalizer[index] = allure_commons.fixture(finalizer, parent_uuid=container_uuid, name=name)
+        except AttributeError as ex:
+            for index, finalizer in enumerate(fixturedef._finalizers or ()):
+                name = '{fixture}::{finalizer}'.format(fixture=fixturedef.argname, finalizer=finalizer.__name__)
+                fixturedef._finalizers[index] = allure_commons.fixture(finalizer, parent_uuid=container_uuid, name=name)
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_fixture_post_finalizer(self, fixturedef):
