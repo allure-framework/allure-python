ae23da945fb1e7dca5284180a359709c226372be
ProSerg <primatvko@gmail.com>
2017-12-05 17:54:18 +0400

    fix finalizers after pytest update and remove py33 form supporters pythons (fixes #181 via #183)

--- allure-pytest/src/listener.py
+++ allure-pytest/src/listener.py
@@ -128,10 +128,10 @@ class AllureListener(object):
                                                stop=now(),
                                                status=get_outcome_status(outcome),
                                                statusDetails=get_outcome_status_details(outcome))
-
-        for index, finalizer in enumerate(fixturedef._finalizer or ()):
+        finalizers = fixturedef._finalizer if hasattr(fixturedef, 'finalizer') else fixturedef._finalizers
+        for index, finalizer in enumerate(finalizers or ()):
             name = '{fixture}::{finalizer}'.format(fixture=fixturedef.argname, finalizer=finalizer.__name__)
-            fixturedef._finalizer[index] = allure_commons.fixture(finalizer, parent_uuid=container_uuid, name=name)
+            finalizers[index] = allure_commons.fixture(finalizer, parent_uuid=container_uuid, name=name)
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_fixture_post_finalizer(self, fixturedef):
