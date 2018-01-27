a3207002352cf9b123b8fc4931593adef0d9c27a
Stas Seliverstov <s.seliverstov@gmail.com>
2018-01-27 16:02:00 +0300

    fix it

--- allure-pytest/test/plugin/register_unregister/master_test.py
+++ allure-pytest/test/plugin/register_unregister/master_test.py
@@ -2,17 +2,18 @@ import os
 import pytest
 
 
-def test_register_unregister():
+def test_register_unregister(tmpdir):
     """
     >>> allure_report = getfixture('allure_report')
     >>> assert_that(allure_report,
     ...             has_test_case('test_register_unregister',
     ...                           with_status('passed')
-    ...              )
+    ...             )
     ... )
     """
     current_dir = os.path.dirname(os.path.realpath(__file__))
     target = os.path.join(current_dir, 'sample.py')
-    assert pytest.main(['--alluredir=/tmp', target]) == 0
-    assert pytest.main(['--alluredir=/tmp', target]) == 0
+    allure_option = '--alluredir={dir}'.format(dir=tmpdir.mkdir('trash').strpath)
+    assert pytest.main([allure_option, target]) == 0
+    assert pytest.main([allure_option, target]) == 0
 
