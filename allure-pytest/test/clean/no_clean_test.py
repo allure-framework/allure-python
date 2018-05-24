ef1ff7d4222696ebc889ac2c092b4c0e97220727
Victor <victor.maryama@typhoon-hil.com>
2018-05-24 20:04:37 +0200

    Changed test name and removed unnecessary things.

--- allure-pytest/test/clean/no_clean_test.py
+++ allure-pytest/test/clean/no_clean_test.py
@@ -1,15 +1,10 @@
-"""
->>> getfixture('inject_matchers')
->>> allure_report = getfixture('allure_report')
-"""
-
-def test_dummy():
+def test_two_runs_no_clean():
     """
     >>> report_fixture = getfixture('allure_report_with_params')
     >>> allure_report_first_run = report_fixture(cache=False)
     >>> allure_report_second_run = report_fixture(cache=False)
     >>> assert_that(allure_report_second_run,
-    ...             has_only_n_test_cases('test_dummy', 2)
+    ...             has_only_n_test_cases('test_two_runs_no_clean', 2)
     ... )
     """
     assert True
