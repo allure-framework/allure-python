ce1d22894a799871463def9e41fcd3711b5da6c6
Victor <victor.maryama@typhoon-hil.com>
2018-05-24 19:44:41 +0200

    Changed test name.

--- allure-pytest/test/steps/outside/run_step_outside_test.py
+++ allure-pytest/test/steps/outside/run_step_outside_test.py
@@ -1,17 +1,12 @@
-"""
->>> getfixture('inject_matchers')
->>> allure_report = getfixture('allure_report')
-"""
-
 from . import init_step
 
 init_step()
 
-def test_dummy():
+def test_pass_with_step_outside():
     """
     >>> allure_report = getfixture('allure_report')
     >>> assert_that(allure_report,
-    ...             has_test_case('test_dummy')
+    ...             has_test_case('test_pass_with_step_outside')
     ... )
     """
     assert True
