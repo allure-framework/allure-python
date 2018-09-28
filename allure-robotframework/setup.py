5153ae457e89b68a4a557f59cadd1465ccb47c87
Stas Seliverstov <s.seliverstov@gmail.com>
2018-09-28 17:39:10 +0300

    bump version

--- allure-robotframework/setup.py
+++ allure-robotframework/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 PACKAGE = "allure-robotframework"
-VERSION = "2.5.1"
+VERSION = "2.5.2"
 
 classifiers = [
     'Development Status :: 5 - Production/Stable',
@@ -16,7 +16,7 @@ classifiers = [
 ]
 
 install_requires = [
-    "allure-python-commons==2.5.1",
+    "allure-python-commons==2.5.2",
 ]
 
 
