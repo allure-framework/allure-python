32d80a05576aaa80fd1a4d9aaa4cdd68219b3c67
Stanislav Seliverstov <s.seliverstov@gmail.com>
2019-02-19 21:51:42 +0300

    improve allure-pytest testing (via #356)

--- allure-pytest/test/integration/pytest_flakes/pytest_flakes_test.py
+++ allure-pytest/test/integration/pytest_flakes/pytest_flakes_test.py
@@ -1,14 +1,30 @@
-import pytest
+import allure
+from hamcrest import assert_that
+from allure_commons_test.report import has_test_case
+from allure_commons_test.result import with_status
 
 
-@pytest.mark.xfail()
-def test_pytest_flakes(allured_testdir, request):
+@allure.issue("352")
+def test_pytest_flakes(allured_testdir):
     """
+    >>> from os.path import *
     >>> def test_pytest_flakes_example():
     ...     assert True
     """
 
-    allured_testdir.parse_docstring_source(request)
+    allured_testdir.parse_docstring_source()
     allured_testdir.run_with_allure("--flakes")
 
-    assert False, allured_testdir.allure_report
+    assert_that(allured_testdir.allure_report,
+                has_test_case("test_pytest_flakes.py",
+                              with_status("broken")
+                              ),
+
+                )
+
+    assert_that(allured_testdir.allure_report,
+                has_test_case("test_pytest_flakes_example",
+                              with_status("passed")
+                              )
+
+                )
