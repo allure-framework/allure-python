248a91029cd5e27d94c808c4499129d11228ba6c
Stanislav Seliverstov <s.seliverstov@gmail.com>
2019-03-17 14:30:35 +0300

    bump version (via #366)

--- allure-python-commons-test/setup.py
+++ allure-python-commons-test/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 PACKAGE = "allure-python-commons-test"
-VERSION = "2.6.0"
+VERSION = "2.6.1"
 
 install_requires = [
     "pyhamcrest>=1.9.0",
--- allure-python-commons/setup.py
+++ allure-python-commons/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 PACKAGE = "allure-python-commons"
-VERSION = "2.6.0"
+VERSION = "2.6.1"
 
 classifiers = [
     'Development Status :: 5 - Production/Stable',
