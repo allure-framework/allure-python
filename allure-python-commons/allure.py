f5c9928279ce57e0004a7911aa72924afcd240fd
Stas Seliverstov <s.seliverstov@gmail.com>
2019-06-26 14:15:33 +0300

    custom suite labels for pytest

--- allure-python-commons/allure.py
+++ allure-python-commons/allure.py
@@ -3,6 +3,7 @@ from allure_commons._allure import description, description_html
 from allure_commons._allure import label
 from allure_commons._allure import severity
 from allure_commons._allure import tag
+from allure_commons._allure import suite, parent_suite, sub_suite
 from allure_commons._allure import epic, feature, story
 from allure_commons._allure import link, issue, testcase
 from allure_commons._allure import Dynamic as dynamic
@@ -18,21 +19,19 @@ __all__ = [
     'description_html',
     'label',
     'severity',
+    'suite',
+    'parent_suite',
+    'sub_suite',
     'tag',
     'epic',
     'feature',
     'story',
-
     'link',
     'issue',
     'testcase',
-
     'step',
-
     'dynamic',
-
     'severity_level',
-
     'attach',
     'attachment_type'
 ]
