6443d52affc92944aa156345e35f1f9c6d2dfef3
Stanislav Seliverstov <s.seliverstov@gmail.com>
2019-10-08 13:14:18 +0300

    bump version to 2.8.6 (via #435)

--- allure-pytest/setup.py
+++ allure-pytest/setup.py
@@ -14,7 +14,7 @@ except (DistributionNotFound, VersionConflict):
     pass
 
 PACKAGE = "allure-pytest"
-VERSION = "2.8.5"
+VERSION = "2.8.6"
 
 classifiers = [
     'Development Status :: 5 - Production/Stable',
@@ -28,7 +28,7 @@ classifiers = [
 install_requires = [
     "pytest>=4.5.0",
     "six>=1.9.0",
-    "allure-python-commons==2.8.5"
+    "allure-python-commons==2.8.6"
 ]
 
 
