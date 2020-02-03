86dc477179fdebddfde037a446dfd63ba50782eb
Lukasz <zakraca@gmail.com>
2020-02-03 20:36:07 +0100

    FIX: #455 'allure-pytest-bdd' report marks feature as 'Fail' even when all steps pass when test is using 'Scenario Outline'

--- allure-pytest-bdd/src/utils.py
+++ allure-pytest-bdd/src/utils.py
@@ -2,6 +2,7 @@ import os
 from uuid import UUID
 from allure_commons.utils import md5
 from allure_commons.model2 import StatusDetails
+from allure_commons.model2 import Status
 from allure_commons.utils import format_exception
 
 
@@ -22,3 +23,11 @@ def get_status_details(exception):
     message = str(exception)
     trace = format_exception(type(exception), exception)
     return StatusDetails(message=message, trace=trace) if message or trace else None
+
+
+def get_pytest_report_status(pytest_report):
+    pytest_statuses = ('failed', 'passed', 'skipped')
+    statuses = (Status.FAILED, Status.PASSED, Status.SKIPPED)
+    for pytest_status, status in zip(pytest_statuses, statuses):
+        if getattr(pytest_report, pytest_status):
+            return status
