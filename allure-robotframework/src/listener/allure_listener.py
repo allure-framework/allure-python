ec6bd98cabe5c27a2aac7f0a23348fdf706ca0b8
Sergey Khomutinin <31664571+skhomuti@users.noreply.github.com>
2020-05-18 16:08:03 +0500

    added allure.labels deco for robotframework (via #487)

--- allure-robotframework/src/listener/allure_listener.py
+++ allure-robotframework/src/listener/allure_listener.py
@@ -7,6 +7,21 @@ class AllureListener(object):
         self.logger = logger
 
     @allure_commons.hookimpl
+    def decorate_as_label(self, label_type, labels):
+        def deco(func):
+            def wrapper(*args, **kwargs):
+                self.add_label(label_type, labels)
+                func(*args, **kwargs)
+            return wrapper
+        return deco
+
+    @allure_commons.hookimpl
+    def add_label(self, label_type, labels):
+        with self.lifecycle.update_test_case() as case:
+            for label in labels if case else ():
+                case.labels.append(Label(label_type, label))
+
+    @allure_commons.hookimpl
     def attach_data(self, body, name, attachment_type, extension):
         self.logger.attach_data(uuid4(), body, name=name, attachment_type=attachment_type, extension=extension)
 
--- allure-robotframework/test/label/testcase_bdd_label.robot
+++ allure-robotframework/test/label/testcase_bdd_label.robot
@@ -5,7 +5,7 @@ Library     ../test_allure_library.py
 
 *** Test Case ***
 Run Without options
-    ${allure_report}   Run Robot With Allure   examples/label/
+    ${allure_report}   Run Robot With Allure   examples/label
     ${test_case}     Should Has Test Case   ${allure_report}   Test Cases With BDD Labels
     Should Has Label  ${test_case}  epic    Tag
     Should Has Label  ${test_case}  feature    Label
