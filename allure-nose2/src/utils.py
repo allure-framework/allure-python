d3c0b732fd97143923ba78f84e43e062dce8a993
Stanislav Seliverstov <s.seliverstov@gmail.com>
2020-10-29 12:06:09 +0300

    nose2 first version (via #513)

--- allure-nose2/src/utils.py
+++ allure-nose2/src/utils.py
@@ -64,12 +64,7 @@ def labels(test):
 
 def name(event):
     full_name = fullname(event)
-    test_params = params(event)
-    allure_name = full_name.split(".")[-1]
-    if test_params:
-        params_str = "-".join([p.value for p in test_params])
-        return "{name}[{params_str}]".format(name=allure_name, params_str=params_str)
-    return allure_name
+    return full_name.split(".")[-1]
 
 
 def fullname(event):
