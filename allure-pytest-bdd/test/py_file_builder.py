aec2b58ae26cdd6d1122f083d5476bad0e8e867e
Denis Bautin <denisbduis@gmail.com>
2021-01-12 00:28:31 +0500

    Create basis for PyFileBuilder, and acceptance.feature

---
+++ allure-pytest-bdd/test/py_file_builder.py
@@ -0,0 +1,16 @@
+
+class PyFileBuilder:
+
+    def __init__(self):
+        self._import_line = None
+        self._file_funcs = []
+
+    def add_imports(self, *modules_names):
+        """ Ex: module_names = ("pytest", ...)"""
+        pass
+
+    def add_func(self, str_func):
+        pass
+
+    def create_file_content(self):
+        pass
