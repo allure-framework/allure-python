364b71f9f63e907a0b902a927a1f5e3d76a2e681
Denis Bautin <denisbduis@gmail.com>
2021-01-12 18:29:45 +0500

    Create base tests on attachments

---
+++ allure-pytest-bdd/test/attachments_tests/tests_for_attachments.py
@@ -0,0 +1,16 @@
+from pytest_bdd import scenario
+
+
+@scenario("attachments_features\\attachment_in_step.feature", "Attachment in Given")
+def test_given_with_attachments():
+    pass
+
+
+@scenario("attachments_features\\attachment_in_step.feature", "Attachment in When")
+def test_when_with_attachments():
+    pass
+
+
+@scenario("attachments_features\\attachment_in_step.feature", "Attachment in Then")
+def test_then_with_attachments():
+    pass
