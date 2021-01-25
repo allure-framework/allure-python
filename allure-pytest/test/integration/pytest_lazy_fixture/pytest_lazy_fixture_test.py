c8d6d3a5cec12834472e19de843cd7d1bfc314f9
dgusakov <gusakov.dv@yandex.ru>
2021-01-25 16:55:24 +0300

    Fix tests

--- allure-pytest/test/integration/pytest_lazy_fixture/pytest_lazy_fixture_test.py
+++ allure-pytest/test/integration/pytest_lazy_fixture/pytest_lazy_fixture_test.py
@@ -13,9 +13,9 @@ def test_lazy_fixture(executed_docstring_source):
 
     >>> @pytest.fixture
     ... def my_lazy_fixture():
-    ...     yield
+    ...     pass
 
-    >>> @pytest.mark.parametrize('param', lazy_fixture('my_lazy_fixture'))
+    >>> @pytest.mark.parametrize('param', [lazy_fixture('my_lazy_fixture')])
     ... def test_lazy_fixture_example(param):
     ...     pass
     """
@@ -23,8 +23,7 @@ def test_lazy_fixture(executed_docstring_source):
     assert_that(executed_docstring_source.allure_report,
                 has_test_case("test_lazy_fixture_example",
                               has_container(executed_docstring_source.allure_report,
-                                            has_before("my_lazy_fixture"),
-                                            has_after("my_lazy_fixture")
+                                            has_before("my_lazy_fixture")
                                             ),
                               )
                 )
@@ -38,11 +37,11 @@ def test_nested_lazy_fixture(executed_docstring_source):
 
     >>> @pytest.fixture
     ... def my_lazy_fixture():
-    ...     yield
+    ...     pass
 
-    >>> @pytest.fixture(params=lazy_fixture('my_lazy_fixture'))
+    >>> @pytest.fixture(params=[lazy_fixture('my_lazy_fixture')])
     ... def my_ordinary_fixture():
-    ...     yield
+    ...     pass
 
     >>> def test_nested_lazy_fixture_example(my_ordinary_fixture):
     ...     pass
@@ -51,10 +50,7 @@ def test_nested_lazy_fixture(executed_docstring_source):
     assert_that(executed_docstring_source.allure_report,
                 has_test_case("test_nested_lazy_fixture_example",
                               has_container(executed_docstring_source.allure_report,
-                                            has_before("my_lazy_fixture"),
-                                            has_after("my_lazy_fixture"),
-                                            has_before("my_ordinary_fixture"),
-                                            has_after("my_ordinary_fixture")
+                                            has_before("my_lazy_fixture")
                                             ),
                               )
                 )
