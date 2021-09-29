9836e5652626c712751820c75f237971c163c0b6
Sergey Khomutinin <31664571+skhomuti@users.noreply.github.com>
2021-09-30 00:41:41 +0500

    Add py38 and py39 to build envs (via #613)

--- allure-nose2/setup.py
+++ allure-nose2/setup.py
@@ -12,6 +12,8 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
 ]
 
 setup_requires = [
