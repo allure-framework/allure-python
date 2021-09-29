4c7a60813314d354d64cb4c73af7b772b627ebb0
Max  麻虾 <229max@users.noreply.github.com>
2021-09-30 03:47:48 +0800

    fix none step_result error (via #608)

--- allure-pytest-bdd/src/pytest_bdd_listener.py
+++ allure-pytest-bdd/src/pytest_bdd_listener.py
@@ -60,7 +60,7 @@ class PytestBDDListener(object):
             test_result.stop = now()
 
     @pytest.hookimpl
-    def pytest_bdd_before_step_call(self, request, feature, scenario, step, step_func, step_func_args):
+    def pytest_bdd_before_step(self, request, feature, scenario, step, step_func):
         parent_uuid = get_uuid(request.node.nodeid)
         uuid = get_uuid(str(id(step)))
         with self.lifecycle.start_step(parent_uuid=parent_uuid, uuid=uuid) as step_result:
