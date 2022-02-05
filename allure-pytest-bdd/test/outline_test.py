a1cb964fbdf666c9a9227a8e3920450873416f58
skhomuti <skhomuti@gmail.com>
2022-02-05 13:28:56 +0500

    Mute pytest-bdd test due to https://github.com/pytest-dev/pytest-bdd/issues/447

--- allure-pytest-bdd/test/outline_test.py
+++ allure-pytest-bdd/test/outline_test.py
@@ -1,6 +1,8 @@
 from pytest_bdd import scenario
+import pytest
 
 
+@pytest.mark.skip(reason="https://github.com/pytest-dev/pytest-bdd/issues/447")
 @scenario("../features/outline.feature", "Scenario outline")
 def test_scenario_outline():
     pass
