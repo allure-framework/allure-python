f4b3f24225c3af6ec668f7a237e4feb2ab943bac
Сафин Альберт Адилевич <safin.a@medindex.ru>
2022-09-30 15:10:04 +0300

    Added test case filtering by `--allure-labels` cmdline option

--- allure-pytest/src/plugin.py
+++ allure-pytest/src/plugin.py
@@ -98,6 +98,20 @@ def pytest_addoption(parser):
                                          help="""Comma-separated list of IDs.
                                          Run tests that have at least one of the specified id labels.""")
 
+    def cf_type(string):
+        type_name, *values = string.split("=")
+        atoms = set(values[0].split(","))
+        return set((type_name, atom) for atom in atoms)
+
+    parser.getgroup("general").addoption('--allure-labels',
+                                         action="append",
+                                         dest="allure_labels",
+                                         metavar="LABELS_SET",
+                                         default=[],
+                                         type=cf_type,
+                                         help="""List of labels to run in format label_name=value1,value2.
+                                         "Run tests that have at least one of the specified labels.""")
+
     def link_pattern(string):
         pattern = string.split(':', 1)
         if not pattern[0]:
@@ -157,11 +171,13 @@ def pytest_configure(config):
 
 
 def select_by_labels(items, config):
+    config.option.allure_labels = set().union(*config.option.allure_labels)
     arg_labels = set().union(config.option.allure_epics,
                              config.option.allure_features,
                              config.option.allure_stories,
                              config.option.allure_ids,
-                             config.option.allure_severities)
+                             config.option.allure_severities,
+                             config.option.allure_labels)
     if arg_labels:
         selected, deselected = [], []
         for item in items:
