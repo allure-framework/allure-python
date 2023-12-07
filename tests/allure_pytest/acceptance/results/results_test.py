23f0070cf82e89042807b0ef28d63b467a0b5ef0
Aleksandr Brodin <sank.brodin@gmail.com>
2023-12-07 17:57:46 +0700

    fix linters errors

--- tests/allure_pytest/acceptance/results/results_test.py
+++ tests/allure_pytest/acceptance/results/results_test.py
@@ -10,15 +10,15 @@ def test_custom_alluredir(allure_pytest_runner: AllurePytestRunner):
     alluredir = allure_pytest_runner.pytester.path
     allure_pytest_runner.in_memory = False
 
-    #run test twice
-    #results of all runs must be in the results directory
+    # run test twice
+    # results of all runs must be in the results directory
     for _ in range(2):
         allure_pytest_runner.run_pytest(
             TEST_FUNC,
             cli_args=["--alluredir", "allure_results"]
         )
-    assert (alluredir /'allure_results').exists()
-    results = AllureReport(alluredir /'allure_results')
+    assert (alluredir / 'allure_results').exists()
+    results = AllureReport(alluredir / 'allure_results')
     assert len(results.test_cases) == 2
 
 
@@ -26,14 +26,14 @@ def test_clean_alluredir(allure_pytest_runner: AllurePytestRunner):
     alluredir = allure_pytest_runner.pytester.path
     allure_pytest_runner.in_memory = False
 
-    #run test twice
-    #results of only last runs must be in the results directory
+    # run test twice
+    # results of only last runs must be in the results directory
     for _ in range(2):
         allure_pytest_runner.run_pytest(
             TEST_FUNC,
             cli_args=["--alluredir", "allure_results", "--clean-alluredir"]
         )
-    results = AllureReport(alluredir /'allure_results')
+    results = AllureReport(alluredir / 'allure_results')
     assert len(results.test_cases) == 1
 
 
@@ -46,12 +46,12 @@ def test_clean_alluredir_with_collectonly(allure_pytest_runner: AllurePytestRunn
         TEST_FUNC,
         cli_args=["--alluredir", "allure_results"]
     )
-    results_before_clean = AllureReport(alluredir /'allure_results')
+    results_before_clean = AllureReport(alluredir / 'allure_results')
     # run test with --collectonly
     allure_pytest_runner.run_pytest(
         TEST_FUNC,
         cli_args=["--alluredir", "allure_results", "--clean-alluredir", "--collectonly"]
     )
     # results should be the same
-    results_after_clean = AllureReport(alluredir /'allure_results')
+    results_after_clean = AllureReport(alluredir / 'allure_results')
     assert results_before_clean.test_cases == results_after_clean.test_cases
--- tests/allure_pytest_bdd/acceptance/results/results_test.py
+++ tests/allure_pytest_bdd/acceptance/results/results_test.py
@@ -39,16 +39,16 @@ def test_custom_alluredir(allure_pytest_bdd_runner: AllurePytestRunner):
     alluredir = allure_pytest_bdd_runner.pytester.path
     allure_pytest_bdd_runner.in_memory = False
 
-    #run test twice
-    #results of all runs must be in the results directory
+    # run test twice
+    # results of all runs must be in the results directory
     for _ in range(2):
         allure_pytest_bdd_runner.run_pytest(
             ("scenario.feature", FEATURE_CONTENT),
             STEPS_CONTENT,
             cli_args=["--alluredir", "allure_results"]
         )
-    assert (alluredir /'allure_results').exists()
-    results = AllureReport(alluredir /'allure_results')
+    assert (alluredir / 'allure_results').exists()
+    results = AllureReport(alluredir / 'allure_results')
     assert len(results.test_cases) == 2
 
 
@@ -56,15 +56,15 @@ def test_clean_alluredir(allure_pytest_bdd_runner: AllurePytestRunner):
     alluredir = allure_pytest_bdd_runner.pytester.path
     allure_pytest_bdd_runner.in_memory = False
 
-    #run test twice
-    #results of only last runs must be in the results directory
+    # run test twice
+    # results of only last runs must be in the results directory
     for _ in range(2):
         allure_pytest_bdd_runner.run_pytest(
             ("scenario.feature", FEATURE_CONTENT),
             STEPS_CONTENT,
             cli_args=["--alluredir", "allure_results", "--clean-alluredir"]
         )
-    results = AllureReport(alluredir /'allure_results')
+    results = AllureReport(alluredir / 'allure_results')
     assert len(results.test_cases) == 1
 
 
@@ -78,7 +78,7 @@ def test_clean_alluredir_with_collectonly(allure_pytest_bdd_runner: AllurePytest
         STEPS_CONTENT,
         cli_args=["--alluredir", "allure_results"]
     )
-    results_before_clean = AllureReport(alluredir /'allure_results')
+    results_before_clean = AllureReport(alluredir / 'allure_results')
     # run test with --collectonly
     allure_pytest_bdd_runner.run_pytest(
         ("scenario.feature", FEATURE_CONTENT),
@@ -86,5 +86,5 @@ def test_clean_alluredir_with_collectonly(allure_pytest_bdd_runner: AllurePytest
         cli_args=["--alluredir", "allure_results", "--clean-alluredir", "--collectonly"]
     )
     # results should be the same
-    results_after_clean = AllureReport(alluredir /'allure_results')
+    results_after_clean = AllureReport(alluredir / 'allure_results')
     assert results_before_clean.test_cases == results_after_clean.test_cases
