2386ff83455ef40ac730087b58e344180127a02e
Anton Shvetsov <antonshvecow@gmail.com>
2024-05-23 19:45:55 +0300

    Added the test for tags in allure-pytest-bdd

--- tests/allure_pytest_bdd/acceptance/results/results_test.py
+++ tests/allure_pytest_bdd/acceptance/results/results_test.py
@@ -17,7 +17,6 @@ STEPS_CONTENT = (
     import allure
     from pytest_bdd import scenario, given, when, then
 
-    @allure.id('1')
     @scenario("scenario.feature", "Simple passed example")
     def test_scenario_passes():
         pass
