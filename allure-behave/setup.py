4f92727d901eaa7d99255f5ffe5736fd06dcf9a8
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-03-14 21:57:25 +0700

    fix: classifier fixes

--- allure-behave/setup.py
+++ allure-behave/setup.py
@@ -12,12 +12,12 @@ classifiers = [
     'Topic :: Software Development :: Testing :: BDD',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
 ]
 
 setup_requires = [
@@ -66,4 +66,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
--- allure-pytest/setup.py
+++ allure-pytest/setup.py
@@ -24,12 +24,12 @@ classifiers = [
     'Topic :: Software Development :: Testing',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
 ]
 
 setup_requires = [
@@ -80,4 +80,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
