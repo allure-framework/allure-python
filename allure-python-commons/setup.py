3ea25a29f2ff9f2ef4b13af172e82aa100394524
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-04-02 21:15:37 +0700

    feat(commons): enable typecheck for Allure API (#850)

--- allure-python-commons/setup.py
+++ allure-python-commons/setup.py
@@ -4,19 +4,19 @@ from setuptools import setup
 PACKAGE = "allure-python-commons"
 
 classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: Apache Software License',
-    'Topic :: Software Development :: Quality Assurance',
-    'Topic :: Software Development :: Testing',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11',
-    'Programming Language :: Python :: 3.12',
-    'Programming Language :: Python :: 3.13',
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Topic :: Software Development :: Quality Assurance",
+    "Topic :: Software Development :: Testing",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
 ]
 
 install_requires = [
@@ -33,10 +33,10 @@ def main():
     setup(
         name=PACKAGE,
         use_scm_version={"root": "..", "relative_to": __file__},
-        setup_requires=['setuptools_scm'],
+        setup_requires=["setuptools_scm"],
         description=(
             "Contains the API for end users as well as helper functions and "
-            "classes to build Allure adapters for Python test frameworks",
+            "classes to build Allure adapters for Python test frameworks"
         ),
         url="https://allurereport.org/",
         project_urls={
@@ -49,13 +49,16 @@ def main():
         keywords="allure reporting report-engine",
         long_description=get_readme("README.md"),
         long_description_content_type="text/markdown",
-        packages=["allure_commons"],
-        package_dir={"allure_commons": 'src'},
+        packages=["allure_commons", "allure"],
+        package_data={
+            "allure": ["py.typed"],
+            "allure_commons": ["py.typed"],
+        },
+        package_dir={"": "src"},
         install_requires=install_requires,
-        py_modules=['allure', 'allure_commons'],
-        python_requires='>=3.6'
+        python_requires=">=3.6"
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
