c3b3197b0e6c90ac0006c686b36d3153bc5cecd4
sharovd <dnsxns@gmail.com>
2025-06-09 17:07:46 +0400

    apply suggestions from code review

--- tests/allure_pytest/acceptance/label/tag/tag_test.py
+++ tests/allure_pytest/acceptance/label/tag/tag_test.py
@@ -1,4 +1,4 @@
-from hamcrest import assert_that, not_
+from hamcrest import assert_that, not_, anything
 from tests.allure_pytest.pytest_runner import AllurePytestRunner
 
 from allure_commons_test.report import has_test_case
@@ -47,7 +47,7 @@ def test_pytest_marker_with_args_is_not_converted_to_allure_tag(
         has_test_case(
             "test_pytest_marker_with_args_is_not_converted_to_allure_tag_example",
             not_(
-                has_tag("marker('cool', 'stuff')")
+                has_tag(anything())
             )
         )
     )
@@ -71,43 +71,7 @@ def test_pytest_marker_with_kwargs_is_not_converted_to_allure_tag(
         has_test_case(
             "test_pytest_marker_with_kwargs_is_not_converted_to_allure_tag_example",
             not_(
-                has_tag("marker(stuff='cool')")
-            )
-        )
-    )
-
-
-def test_pytest_multiple_simple_and_reserved_markers_to_allure_tags(
-        allure_pytest_runner: AllurePytestRunner
-):
-    """
-    >>> import pytest
-
-    >>> @pytest.mark.usermark1
-    ... @pytest.mark.usermark2
-    ... @pytest.mark.parametrize("param", ["foo"])
-    ... @pytest.mark.skipif(False, reason="reason2")
-    ... @pytest.mark.skipif(False, reason="reason1")
-    ... def test_pytest_multiple_simple_and_reserved_markers_to_allure_tags_example(param):
-    ...     pass
-    """
-
-    allure_results = allure_pytest_runner.run_docstring()
-
-    assert_that(
-        allure_results,
-        has_test_case(
-            "test_pytest_multiple_simple_and_reserved_markers_to_allure_tags_example[foo]",
-            has_tag("usermark1"),
-            has_tag("usermark2"),
-            not_(
-                has_tag("skipif(False, reason='reason1')")
-            ),
-            not_(
-                has_tag("skipif(False, reason='reason2')")
-            ),
-            not_(
-                has_tag("parametrize('param', ['foo'])")
+                has_tag(anything())
             )
         )
     )
@@ -131,7 +95,7 @@ def test_pytest_reserved_marker_usefixtures_is_not_converted_to_allure_tag(
         has_test_case(
             "test_pytest_reserved_marker_usefixtures_is_not_converted_to_allure_tag_example",
             not_(
-                has_tag("usefixtures('test_fixture')")
+                has_tag(anything())
             )
         )
     )
@@ -155,7 +119,7 @@ def test_pytest_reserved_marker_filterwarnings_is_not_converted_to_allure_tag(
         has_test_case(
             "test_pytest_reserved_marker_filterwarnings_is_not_converted_to_allure_tag_example",
             not_(
-                has_tag("filterwarnings('ignore:val')")
+                has_tag(anything())
             )
         )
     )
@@ -179,7 +143,7 @@ def test_pytest_reserved_marker_skip_is_not_converted_to_allure_tag(
         has_test_case(
             "test_pytest_reserved_marker_skip_is_not_converted_to_allure_tag_example",
             not_(
-                has_tag("skip(reason='reason')")
+                has_tag(anything())
             )
         )
     )
@@ -203,7 +167,7 @@ def test_pytest_reserved_marker_skipif_is_not_converted_to_allure_tag(
         has_test_case(
             "test_pytest_reserved_marker_skipif_is_not_converted_to_allure_tag_example",
             not_(
-                has_tag("skipif(False, reason='reason')")
+                has_tag(anything())
             )
         )
     )
@@ -227,7 +191,7 @@ def test_pytest_reserved_marker_xfail_is_not_converted_to_allure_tag(
         has_test_case(
             "test_pytest_reserved_marker_xfail_is_not_converted_to_allure_tag_example",
             not_(
-                has_tag("xfail(reason='this is unexpect pass')")
+                has_tag(anything())
             )
         )
     )
@@ -251,31 +215,7 @@ def test_pytest_reserved_marker_parametrize_is_not_converted_to_allure_tag(
         has_test_case(
             "test_pytest_reserved_marker_parametrize_is_not_converted_to_allure_tag_example[foo]",
             not_(
-                has_tag("parametrize('param', ['foo'])")
+                has_tag(anything())
             )
-        )
-    )
-
-
-def test_pytest_simple_markers_utf_encoding_are_converted_to_allure_tags(
-        allure_pytest_runner: AllurePytestRunner
-):
-    """
-    >>> import pytest
-
-    >>> @pytest.mark.классная
-    >>> @pytest.mark.штука
-    ... def test_pytest_simple_markers_utf_encoding_are_converted_to_allure_tags_example():
-    ...     pass
-    """
-
-    allure_results = allure_pytest_runner.run_docstring()
-
-    assert_that(
-        allure_results,
-        has_test_case(
-            "test_pytest_simple_markers_utf_encoding_are_converted_to_allure_tags_example",
-            has_tag("классная"),
-            has_tag("штука")
         )
     )
