83cf085b0fe576ff8af6d91f7b1f1d9882227cc6
El'dar Vershinin <vershinineldar@yandex.ru>
2026-01-06 02:26:35 +0300

    feat(allure-pytest): allow to add parameter directly in step via allure.add_parameter

--- allure-python-commons/src/allure/__init__.py
+++ allure-python-commons/src/allure/__init__.py
@@ -9,6 +9,7 @@ from allure_commons._allure import epic, feature, story
 from allure_commons._allure import link, issue, testcase
 from allure_commons._allure import Dynamic as dynamic
 from allure_commons._allure import step
+from allure_commons._allure import add_parameter
 from allure_commons._allure import attach
 from allure_commons._allure import manual
 from allure_commons.types import Severity as severity_level
@@ -35,6 +36,7 @@ __all__ = [
     "testcase",
     "manual",
     "step",
+    "add_parameter",
     "dynamic",
     "severity_level",
     "attach",
