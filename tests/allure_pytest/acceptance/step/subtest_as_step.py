0a8ed74b06425c9e1e10f5f9f69289c7148b9d23
Aleksandr Brodin <sank.brodin@gmail.com>
2026-01-28 13:42:49 +0700

    support subtest as step

---
+++ tests/allure_pytest/acceptance/step/subtest_as_step.py
@@ -0,0 +1,42 @@
+from hamcrest import assert_that
+from tests.allure_pytest.pytest_runner import AllurePytestRunner
+
+from allure_commons_test.report import has_test_case
+from allure_commons_test.result import has_step
+from allure_commons_test.result import with_status
+from allure_commons_test.result import with_message_contains
+from allure_commons_test.result import has_status_details
+
+
+def test_with_subtest(allure_pytest_runner: AllurePytestRunner):
+    """
+    >>> import allure
+    >>> import pytest
+    >>> @pytest.mark.skipif("pytest.__version__[0] < '9'")
+    >>> def test_with_subtest(subtests):
+    ...    with subtests.test(msg='Some failed subtest'):
+    ...        assert False, 'Some error'
+    ...    with allure.step('Next step'):
+    ...        pass
+    """
+
+    allure_results = allure_pytest_runner.run_docstring()
+
+    assert_that(
+        allure_results,
+        has_test_case(
+            "test_with_subtest",
+            has_step(
+                "Some failed subtest",
+                with_status("failed"),
+                has_status_details(
+                    with_message_contains("Some error")
+                )
+            ),
+            has_step(
+                "Next step",
+                with_status("passed")
+            ),
+            with_status("failed")
+        )
+    )
